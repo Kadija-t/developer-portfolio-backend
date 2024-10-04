2dae84ee3608ff5cd33fe694e34e9a6e87329f85
Kadija T <hmani.kadija11@gmail.com>
2024-10-04 18:12:25 +0200

    updating server.js

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env('APP_KEYS', 'toBeModified1,toBeModified2').split(','),
+    keys: env('APP_KEYS', 'pass1,pass2').split(','),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
