b9c030a51b3bc6bdc5a60157c7e4e53f209d80e2
Kadija T <hmani.kadija11@gmail.com>
2024-10-16 13:05:25 +0200

    updating strapi

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,18 +1,18 @@
-import type { Struct, Schema } from '@strapi/strapi';
+import type { Schema, Attribute } from '@strapi/strapi';
 
-export interface TagTag extends Struct.ComponentSchema {
+export interface TagTag extends Schema.Component {
   collectionName: 'components_tag_tags';
   info: {
     displayName: 'Tag';
   };
   attributes: {
-    title: Schema.Attribute.String;
+    title: Attribute.String;
   };
 }
 
-declare module '@strapi/strapi' {
-  export module Public {
-    export interface ComponentSchemas {
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
       'tag.tag': TagTag;
     }
   }
